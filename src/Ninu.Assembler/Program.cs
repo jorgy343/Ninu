fc73892c86ea7057e5e16266b47b18cdfd0461f6
Alex Jorgenson <jorgy343@gmail.com>
2020-12-05 19:09:39 -0600

    Added a lot of the core for the assembler.

---
+++ src/Ninu.Assembler/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ninu.Assembler.Library;
+
+namespace Ninu.Assembler
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var compiler = new Compiler();
+
+            compiler.Assemble();
+        }
+    }
+}
