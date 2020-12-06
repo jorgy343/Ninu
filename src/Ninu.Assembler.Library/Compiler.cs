0f89f4821bc54dfc3fdc1354c096b729610e204d
Alex Jorgenson <jorgy343@gmail.com>
2020-12-05 19:09:39 -0600

    Added a lot of the core for the assembler.

---
+++ src/Ninu.Assembler.Library/Compiler.cs
@@ -0,0 +1,31 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Ninu.Assembler.Antlr;
+
+namespace Ninu.Assembler.Library
+{
+    public class Compiler
+    {
+        public void Assemble()
+        {
+            var input = @"
+                const orig = $32 * 4
+                origin(orig)
+
+                a:
+                and b:lo
+                ";
+
+            var lexer = new Asm6502Lexer(new AntlrInputStream(input));
+            var parser = new Asm6502Parser(new CommonTokenStream(lexer));
+
+            var context = parser.prog();
+
+            // Walk it and attach our listener
+            var walker = new ParseTreeWalker();
+            var listener = new Listener();
+
+            walker.Walk(listener, context);
+        }
+    }
+}
